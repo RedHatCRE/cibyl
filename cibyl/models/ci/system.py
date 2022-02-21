bb6fb9033b902b4dd475cf436b22cbbca7444be5
jgilaber <jgilaber@redhat.com>
2022-02-21 12:10:41 +0100

    Use argument objects in Attribute values

--- cibyl/models/ci/system.py
+++ cibyl/models/ci/system.py
@@ -15,6 +15,7 @@
 from cibyl.models.attribute import AttributeValue, AttributeListValue
 from cibyl.models.ci.job import Job
 from cibyl.models.ci.pipeline import Pipeline
+from cibyl.cli.argument import Argument
 
 
 class System:
@@ -24,23 +25,24 @@ class System:
     """
 
     def __init__(self, name: str, system_type: str):
-        self.name = AttributeValue(name="name", type=str, value=name,
-                                   arguments=['--system-name'])
-        self.type = AttributeValue(name="type", type=str, value=system_type,
-                                   arguments=['--system-type'])
-        self.pipelines = AttributeListValue(name="pipelines", type=Pipeline,
-                                            arguments=['--pipelines'])
-        self.jobs = AttributeListValue(name="jobs", type=Job,
-                                       arguments=['--jobs'])
+        self.name = AttributeValue(name="name", attr_type=str, value=name,
+                                   arguments=[Argument(name='--system-name',
+                                              arg_type=str,
+                                              description="Name of the system")])
+        self.type = AttributeValue(name="type", attr_type=str, value=system_type,
+                                   arguments=[Argument(name='--system-type',
+                                              arg_type=str,
+                                              description="Type of the system")])
+        self.jobs = AttributeListValue(name="jobs", attr_type=Job,
+                                       arguments=[Argument(name='--jobs',
+                                                  arg_type=str,
+                                                  description="Jobs of the system")])
 
     def __str__(self):
         return f"System {self.name.value} of type {self.type.value}"
 
     def add_job(self, job):
         self.jobs.append(job)
-
-    def add_pipeline(self, pipeline):
-        self.pipelines.append(pipeline)
 
 
 class ZuulSystem(System):
@@ -49,6 +51,13 @@ class ZuulSystem(System):
     """
     def __init__(self, name):
         super(ZuulSystem, self).__init__(name, "zuul")
+        self.pipelines = AttributeListValue(name="pipelines", attr_type=Pipeline,
+                                            arguments=[Argument(name='--jobs',
+                                                       arg_type=str,
+                                                       description="Jobs of the system")])
+
+    def add_pipeline(self, pipeline):
+        self.pipelines.append(pipeline)
 
 
 class JenkinsSystem(System):
