37f3128c5b6c123fde7d5b8e3c8bfacb7121abf7
Adrian Fusco Arnejo <afuscoar@redhat.com>
2022-02-23 12:55:56 +0100

    Elasticsearch OSP classes
    - 'ElasticSearchClient' class used for connecting to any elasticsearch instance.
    - 'ElasticSearchOSPFilter' class that use an elasticsearch client to perform queries.
    
    It can be used to perform queries based on match:
    
    And queries based on regex:
    
    Note: Simple client for simple filtering. It should be improved.

---
+++ cibyl/sources/elastic/client.py
@@ -0,0 +1,72 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+
+import logging
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import ConnectionError, ConnectionTimeout
+from sys import exit
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ElasticSearchClient:
+
+    def __init__(self: object,
+                 host: str = "http://localhost",
+                 port: int = 9200) -> None:
+        self.host = f"{host}:{port}"
+
+    def connect(self: object) -> Elasticsearch:
+        """This is used for connecting to the ElasticSearch instance.
+
+        :param host: Elasticsearch host including the scheme
+        :type host: str, optional
+        :param port: Port for sending REST requests to the instance
+        :type port: str, optional
+        :return: Elasticsearch module instance
+        :rtype: Elasticsearch
+        :raises: ConnectionTimeout:
+                 If the connection attempt reach the max time
+        :raises: ConnectionError:
+                 If exists an handled connection error
+        :raises: Exception:
+                 If exists an unhandled connection error
+        """
+        try:
+            es = Elasticsearch(self.host)
+            es.ping()
+        except ConnectionTimeout as e:
+            LOG.error(f"Timeout connection to ElasticSearch. Details: {e}")
+            exit(1)
+        except ConnectionError as e:
+            LOG.error(f"Connection error to ElasticSearch. Details: {e}")
+            exit(1)
+        except Exception as e:
+            LOG.error(f"An unknown error occurred connecting to ElasticSearch. \
+                        Details: {e}")
+            exit(1)
+        LOG.info("Connection to ElasticSearch successful")
+        return es
+
+
+def main():
+    es = ElasticSearchClient().connect()
+    print(es.cat.health())
+
+
+if __name__ == "__main__":
+    main()
---
+++ cibyl/sources/elastic/filter.py
@@ -0,0 +1,142 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+
+import argparse
+import logging
+from cibyl.sources.elastic.client import ElasticSearchClient
+from sys import exit
+
+LOG = logging.getLogger(__name__)
+
+
+class ElasticSearchOSPFilter:
+
+    def __init__(self: object, elastic_client: object) -> None:
+        self.es = elastic_client
+
+    def get_job_by_name(self: object, query_data: dict):
+        """Get all the job information with an exact match
+
+        :param query_data: Information required for the query:
+                Index, Search key and Search Value.
+        :type query_data: dict
+        """
+        query_body = {
+           'query': {
+                'term': {
+                    f"{query_data['key']}.keyword": {
+                        'value': query_data['value']
+                    }
+                }
+            }
+        }
+        hits = self.__query_get_hits(query_data['index'], query_body)
+        print(hits)
+
+    def get_jobs_by_regex(self: object, query_data: dict):
+        """Get all the jobs that match the provided regex string
+
+        :param query_data: Information required for the query:
+                Index, Search key and Search Value.
+        :type query_data: dict
+        """
+        query_body = {
+            'query': {
+                'regexp': {
+                    query_data['key']: {
+                        'value': query_data['value']
+                    }
+                }
+            }
+        }
+        hits = self.__query_get_hits(query_data['index'], query_body)
+        print(hits)
+
+    def __query_get_hits(self: object, index: str, query: dict) -> list:
+        """This method is used to perform the search query to ElasticSearch
+        and get all the hits
+
+        :param index: Index
+        :type index: str
+        :param query: Index and query to perform
+        :type query: dict
+        :return: List of hits.
+        """
+        try:
+            response = self.es.search(
+                index=index,
+                body=query
+            )
+            LOG.info("Query performed, index: {index}, body: {query}")
+        except Exception as e:
+            LOG.error(f"Query error. Details: {e}")
+            exit(1)
+        return response['hits']['hits']
+
+
+def parse_arguments() -> object:
+    parser = argparse.ArgumentParser(
+        description='Tool for filters test results anomalies in order to '
+                    'compare them separately from the accepted delta range.')
+    parser.add_argument('--host',
+                        help='Elasticsearch host including scheme',
+                        type=str,
+                        default="http://localhost",
+                        dest='host')
+    parser.add_argument('--port',
+                        help='Elasticsearch port for sending REST requests',
+                        type=int,
+                        default=9200,
+                        dest='port')
+    parser.add_argument('--index',
+                        help='Index for searching queries',
+                        required=True,
+                        dest='index')
+    parser.add_argument('--type',
+                        help='Type of search. String or regex string',
+                        type=str,
+                        choices=['string', 'regex'],
+                        required=True,
+                        dest='type')
+    parser.add_argument('--key',
+                        help='key',
+                        required=True,
+                        dest='key')
+    parser.add_argument('--value',
+                        help='value',
+                        required=True,
+                        dest='value')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    elastic_client = ElasticSearchClient(args.host, args.port).connect()
+    elastic_filter = ElasticSearchOSPFilter(elastic_client)
+    query_data = {
+        'index': args.index,
+        'type': args.type,
+        'key': args.key,
+        'value': args.value,
+    }
+    if query_data['type'] == 'string':
+        elastic_filter.get_job_by_name(query_data)
+    else:
+        elastic_filter.get_jobs_by_regex(query_data)
+
+
+if __name__ == "__main__":
+    main()
