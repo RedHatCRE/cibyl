f55516aaef4f7b5bfc247b53dc2107909ee32962
jgilaber <jgilaber@redhat.com>
2022-03-17 14:16:47 +0100

    Add Jenkins Job Builder source to SourceFactory

--- cibyl/sources/source_factory.py
+++ cibyl/sources/source_factory.py
@@ -16,6 +16,7 @@
 from enum import Enum
 
 from cibyl.sources.jenkins import Jenkins
+from cibyl.sources.jenkins_job_builder import JenkinsJobBuilder
 
 
 class SourceType(str, Enum):
@@ -24,6 +25,7 @@ class SourceType(str, Enum):
     JENKINS = 'jenkins'
     ZUUL = 'zuul'
     ELASTICSEARCH = 'elasticsearch'
+    JENKINS_JOB_BUILDER = 'jenkins_job_builder'
 
 
 class SourceFactory:
@@ -53,5 +55,8 @@ class SourceFactory:
 
         if source_type == SourceType.ELASTICSEARCH:
             return None
+
+        if source_type == SourceType.JENKINS_JOB_BUILDER:
+            return JenkinsJobBuilder(name=name, **kwargs)
 
         raise NotImplementedError(f"Unknown source type '{source_type}'")
