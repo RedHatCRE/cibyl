1efd297362b02661147d165d7c8691729ec39ba3
Jorge San Emeterio <jsanemet@redhat.com>
2022-03-18 12:09:48 +0100

    Extracting source validity check.

--- tests/sources/test_sources.py
+++ tests/sources/test_sources.py
@@ -14,7 +14,7 @@
 #    under the License.
 """
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, call
 
 from cibyl.exceptions.source import TooManyValidSources
 from cibyl.sources.source import Source
@@ -23,20 +23,29 @@ from cibyl.sources.source import Source
 class TestGetSourceMethod(TestCase):
     """Tests for the get_source_method static function."""
 
-    def test_no_more_than_two_valid_sources_allowed(self):
+    @patch('cibyl.sources.source.Source.is_source_valid')
+    def test_no_more_than_two_valid_sources_allowed(self, valid_source):
         """Checks that only a single source is allowed to provide the
         desired function.
+
+        :param valid_source: Mock for "is_source_valid"
+        :type valid_source: :class:`Mock`
         """
+        desired_func = 'func'
+
         source1 = Mock()
         source2 = Mock()
 
-        source1.func = Mock()
-        source2.func = Mock()
+        valid_source.return_value = True
 
-        self.assertRaises(
-            TooManyValidSources,
-            Source.get_source_method,
-            'system',
-            [source1, source2],
-            'func'
-        )
+        with self.assertRaises(TooManyValidSources):
+            Source.get_source_method(
+                'system',
+                [source1, source2],
+                desired_func
+            )
+
+        valid_source.assert_has_calls([
+            call(source1, desired_func),
+            call(source2, desired_func)
+        ])
