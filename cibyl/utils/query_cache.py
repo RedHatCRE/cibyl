df0dcf6cca4caabcec2f3f159bd491f76fe3e3b7
jgilaber <jgilaber@redhat.com>
2022-03-24 15:58:32 +0100

    Add a QueriesCache class to avoid repeating calls
    If two arguments of the same level and with the same function associated
    are used, it would result in a two calls to the same method. This simple
    cache will keep track of these calls and avoid wasting time.

---
+++ cibyl/utils/query_cache.py
@@ -0,0 +1,57 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+
+
+class QueriesCache:
+    """Cache of source queries that have been performed. This cache will allow
+    us to avoid sending multiple times the same queries if there are multiple
+    arguments that are associated with the same function (for example --builds
+    and --build-status or --jobs and --jobs-url).
+    """
+    def __init__(self):
+        self.cache = set()
+
+    def _method_information_tuple(self, source_method):
+        """Obtain a tuple representation in the format (source_name,
+        method_name) from the input source_method.
+
+        :param source_method: Source method that is used
+        :type source_method: method
+        :returns: The internal representation of the source_method in the cache
+        :rtype: tuple(str, str)
+        """
+        source = source_method.__self__
+        method_name = source_method.__name__
+        return (source.name, method_name)
+
+    def has_been_called(self, source_method):
+        """Check whether a particular source method has already been called
+        before.
+
+        :param source_method: Source method that is used
+        :type source_method: method
+        :returns: Whether the source method has been called before
+        :rtype: bool
+        """
+        return self._method_information_tuple(source_method) in self.cache
+
+    def add_call(self, source_method):
+        """Add a particular source method to the call cache.
+
+        :param source_method: Source method that is used
+        :type source_method: method
+        """
+        self.cache.add(self._method_information_tuple(source_method))
---
+++ tests/utils/test_queries_cache.py
@@ -0,0 +1,56 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+from unittest import TestCase
+
+from cibyl.sources.jenkins import Jenkins
+from cibyl.sources.zuul.source import Zuul
+from cibyl.utils.query_cache import QueriesCache
+
+
+class TestQueriesCache(TestCase):
+    """Test QueriesCache class."""
+
+    def setUp(self):
+        self.cache = QueriesCache()
+        self.jenkins = Jenkins(url="", name="jenkins_source")
+        self.zuul = Zuul(api=None, name="zuul_source", driver="zuul", url="")
+
+    def test_add_call(self):
+        """Test add_call method."""
+        jenkins_get_jobs = self.jenkins.get_jobs
+        self.cache.add_call(jenkins_get_jobs)
+        self.assertEqual(self.cache.cache,
+                         set([("jenkins_source", "get_jobs")]))
+
+    def test_add_multiple_calls(self):
+        """Test add_call method."""
+        jenkins_get_jobs = self.jenkins.get_jobs
+        zuul_get_builds = self.zuul.get_builds
+        self.cache.add_call(jenkins_get_jobs)
+        self.cache.add_call(zuul_get_builds)
+        self.assertEqual(self.cache.cache,
+                         set([("jenkins_source", "get_jobs"),
+                              ("zuul_source", "get_builds")
+                              ]))
+
+    def test_has_been_called(self):
+        """Test has_been_called method."""
+        jenkins_get_jobs = self.jenkins.get_jobs
+        zuul_get_builds = self.zuul.get_builds
+        self.cache.add_call(jenkins_get_jobs)
+        self.cache.add_call(zuul_get_builds)
+        self.assertTrue(self.cache.has_been_called(jenkins_get_jobs))
+        self.assertTrue(self.cache.has_been_called(zuul_get_builds))
