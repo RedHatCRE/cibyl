e28c5efbf392fe7ec28676b54d8cd3977d3058c3
Jorge San Emeterio <jsanemet@redhat.com>
2022-05-10 14:36:57 +0200

    Adding a specific Job for zuul that contains variants.

---
+++ cibyl/models/ci/zuul/job.py
@@ -0,0 +1,100 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+from overrides import overrides
+
+from cibyl.models.attribute import AttributeListValue, AttributeDictValue
+from cibyl.models.ci.job import Job as BaseJob
+from cibyl.models.model import Model
+
+
+class Job(BaseJob):
+    """
+    @DynamicAttrs: Ignores attributes added on runtime
+    """
+
+    class Variant(Model):
+        """
+        @DynamicAttrs: Ignores attributes added on runtime
+        """
+        API = {
+            'parent': {
+                'attr_type': str,
+                'arguments': []
+            },
+            'description': {
+                'attr_type': str,
+                'arguments': []
+            },
+            'branches': {
+                'attr_type': str,
+                'attribute_value_class': AttributeListValue,
+                'arguments': []
+            },
+            'variables': {
+                'attr_type': str,
+                'attribute_value_class': AttributeDictValue,
+                'arguments': []
+            }
+        }
+
+        def __init__(self,
+                     parent,
+                     description=None,
+                     branches=None,
+                     variables=None):
+            super().__init__(
+                {
+                    'parent': parent,
+                    'description': description,
+                    'branches': branches,
+                    'variables': variables
+                }
+            )
+
+        def __eq__(self, other):
+            if not isinstance(other, Job.Variant):
+                return False
+
+            if self is other:
+                return True
+
+            return \
+                self.parent == other.parent and \
+                self.description == other.description and \
+                self.branches == other.branches and \
+                self.variables == other.variables
+
+    API = {
+        **BaseJob.API,
+        'variants': {
+            'attr_type': Variant,
+            'attribute_value_class': AttributeListValue,
+            'arguments': []
+        }
+    }
+
+    def __init__(self, name, url, variants=None, builds=None):
+        super().__init__(name, url, builds, variants=variants)
+
+    @overrides
+    def merge(self, other):
+        super().merge(other)
+
+        for variant in other.variants:
+            self.add_variant(variant)
+
+    def add_variant(self, variant):
+        self.variants.append(variant)
---
+++ tests/unit/models/ci/zuul/test_job.py
@@ -0,0 +1,102 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+from unittest import TestCase
+from unittest.mock import Mock
+
+from cibyl.models.attribute import AttributeListValue, AttributeValue, \
+    AttributeDictValue
+from cibyl.models.ci.zuul.job import Job
+
+
+class TestJob(TestCase):
+    """Tests for :class:`Job`.
+    """
+
+    def test_has_variants_attribute(self):
+        """Checks that a 'variants' attribute is added to the object.
+        """
+        job = Job('name', 'url')
+
+        self.assertIsInstance(job.variants, AttributeListValue)
+
+    def test_add_variant(self):
+        """Checks that some random variant can be added to the model.
+        """
+        variant = Mock()
+
+        job = Job('name', 'url')
+        job.add_variant(variant)
+
+        self.assertIn(variant, job.variants)
+
+    def test_add_variants_during_merge(self):
+        """Checks that the job will copy the variants from another during
+        merge.
+        """
+        variant = Mock()
+
+        job1 = Job('name', 'url')
+        job2 = Job('name', 'url')
+
+        job2.add_variant(variant)
+        job1.merge(job2)
+
+        self.assertIn(variant, job1.variants)
+
+
+class TestVariant(TestCase):
+    """Tests for :class:`Job.Variant`.
+    """
+
+    def test_attributes(self):
+        """Checks that the variant has all its attributes.
+        """
+        variant = Job.Variant('parent')
+
+        self.assertIsInstance(variant.parent, AttributeValue)
+        self.assertIsInstance(variant.description, AttributeValue)
+        self.assertIsInstance(variant.branches, AttributeListValue)
+        self.assertIsInstance(variant.variables, AttributeDictValue)
+
+    def test_equality_by_type(self):
+        """Checks that two objects of different type are not equal.
+        """
+        variant = Job.Variant('parent')
+        other = Mock()
+
+        self.assertNotEqual(other, variant)
+
+    def test_equality_by_reference(self):
+        """Checks that two objects are equal if they are pointed by the same
+        reference.
+        """
+        variant = Job.Variant('parent')
+        other = variant
+
+        self.assertEqual(other, variant)
+
+    def test_equality_by_content(self):
+        """Checks that two objects are equal if they have the same contents.
+        """
+        parent = 'parent'
+        description = 'description'
+        branches = ['branch1', 'branch2']
+        variables = {'var1': 'val1'}
+
+        variant1 = Job.Variant(parent, description, branches, variables)
+        variant2 = Job.Variant(parent, description, branches, variables)
+
+        self.assertEqual(variant2, variant1)
