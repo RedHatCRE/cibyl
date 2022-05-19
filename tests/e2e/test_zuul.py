5291e4defe5801974ba7f4dcd723609fe0132f18
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-05-19 12:14:59 +0200

    Adding query composition for Zuul source (#257)
    * Have each query fetch the higher level model if they have been requested explictly.
    
    * Organizin zuul e2e test cases
    
    * Fixing deprecation warning.
    
    * Adding test cases
    
    * Adding test cases that check query composition.

--- tests/e2e/test_zuul.py
+++ tests/e2e/test_zuul.py
@@ -21,10 +21,11 @@ from tests.e2e.containers.zuul import OpenDevZuulContainer
 from tests.e2e.fixtures import EndToEndTest
 
 
-class TestZuul(EndToEndTest):
-    """Tests queries regarding the Zuul source.
+class TestQueryLevel(EndToEndTest):
+    """Tests all the different options available for each of the query
+    levels available on a zuul source. For example: fetching all jobs,
+    fetching jobs by URL, by name...
     """
-
     zuul = OpenDevZuulContainer()
 
     @classmethod
@@ -81,7 +82,7 @@ class TestZuul(EndToEndTest):
         main()
 
         self.assertIn(
-            "Total projects found in query for tenant 'example-tenant': 4",
+            "Total projects found in query for tenant 'example-tenant': 3",
             self.output
         )
 
@@ -131,45 +132,6 @@ class TestZuul(EndToEndTest):
 
         self.assertIn(
             "URL: http://localhost:9000/t/example-tenant/project/test1",
-            self.output
-        )
-
-    def test_no_jobs_on_tenant_query(self):
-        """Checks that no 'Jobs found in tenant...' string is printed for a
-        '--tenants' query.
-        """
-        sys.argv = [
-            '',
-            '--config', 'tests/e2e/data/configs/zuul.yaml',
-            '-f', 'text',
-            '-vv',
-            '--tenants', '^(example-tenant)$'
-        ]
-
-        main()
-
-        self.assertNotIn(
-            "Total jobs found in query for tenant 'example-tenant':",
-            self.output
-        )
-
-    def test_no_jobs_on_projects_query(self):
-        """Checks that no 'Jobs found in tenant...' string is printed for a
-        '--projects' query.
-        """
-        sys.argv = [
-            '',
-            '--config', 'tests/e2e/data/configs/zuul.yaml',
-            '-f', 'text',
-            '-vv',
-            '--tenants', '^(example-tenant)$',
-            '--projects'
-        ]
-
-        main()
-
-        self.assertNotIn(
-            "Total jobs found in query for tenant 'example-tenant':",
             self.output
         )
 
@@ -241,7 +203,32 @@ class TestZuul(EndToEndTest):
             self.output
         )
 
-    def test_job_variants(self):
+    def test_get_job_url(self):
+        """Checks that "-v" will print a job's URL.
+        """
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '--tenants', '^(example-tenant)$',
+            '--jobs', 'build-docker-image',
+            '-v'
+        ]
+
+        main()
+
+        self.assertIn(
+            "Job: build-docker-image",
+            self.output
+        )
+
+        self.assertIn(
+            "URL: "
+            "http://localhost:9000/t/example-tenant/job/build-docker-image",
+            self.output
+        )
+
+    def test_get_job_variants(self):
         """Checks retrieved variants by "--jobs --variants" flag.
         """
         sys.argv = [
@@ -269,6 +256,167 @@ class TestZuul(EndToEndTest):
             expected.build(),
             self.output
         )
+
+
+class TestQueryComposing(EndToEndTest):
+    """Tests that multiple queries on the same request end with an output
+    that combines the result of all them. For example: 'cibyl
+    --tenants --projects projectA' will fetch all tenants and get
+    information on only the 'projectA' project.
+    """
+    zuul = OpenDevZuulContainer()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.zuul.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.zuul.stop()
+
+    def test_tenants_with_project(self):
+        """Checks that '--tenants --project projectA' gets you all tenants
+        as well."""
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '--tenants',
+            '--projects', '^test2$'
+        ]
+
+        main()
+
+        expected = IndentedTextBuilder()
+        expected.add('Environment: env_1', 0)
+        expected.add('System: zuul_system', 1)
+        expected.add('Tenant: example-tenant', 2)
+        expected.add('Tenant: example-tenant-2', 2)
+        expected.add('Projects: ', 3)
+        expected.add('Project: test2', 4)
+
+        self.assertIn(expected.build(), self.output)
+
+    def test_tenants_with_jobs(self):
+        """Checks that '--tenants --project projectA' gets you all tenants
+        as well."""
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '--tenants',
+            '--jobs', '^build-docker-image$'
+        ]
+
+        main()
+
+        expected1 = IndentedTextBuilder()
+        expected1.add('Environment: env_1', 0)
+        expected1.add('System: zuul_system', 1)
+        expected1.add('Tenant: example-tenant', 2)
+        expected1.add('Jobs: ', 3)
+        expected1.add('Job: build-docker-image', 4)
+
+        expected2 = IndentedTextBuilder()
+        expected2.add('Tenant: example-tenant-2', 2)
+
+        self.assertIn(expected1.build(), self.output)
+        self.assertIn(expected2.build(), self.output)
+
+    def test_projects_with_jobs(self):
+        """Checks that '--tenants --project projectA' gets you all tenants
+        as well."""
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '--projects', '^test2$',
+            '--jobs', '^build-docker-image$'
+        ]
+
+        main()
+
+        expected1 = IndentedTextBuilder()
+        expected1.add('Tenant: example-tenant-2', 2)
+        expected1.add('Projects: ', 3)
+        expected1.add('Project: test2', 4)
+
+        expected2 = IndentedTextBuilder()
+        expected2.add('Tenant: example-tenant', 2)
+        expected2.add('Jobs: ', 3)
+        expected2.add('Job: build-docker-image', 4)
+
+        self.assertIn(expected1.build(), self.output)
+        self.assertIn(expected2.build(), self.output)
+
+
+class TestOutputFormatting(EndToEndTest):
+    """Tests that verify specific conditions that shall be met on Cibyl's
+    output.
+    """
+    zuul = OpenDevZuulContainer()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.zuul.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.zuul.stop()
+
+    def test_no_jobs_on_tenant_query(self):
+        """Checks that no 'Jobs found in tenant...' string is printed for a
+        '--tenants' query.
+        """
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '-vv',
+            '--tenants', '^(example-tenant)$'
+        ]
+
+        main()
+
+        self.assertNotIn(
+            "Total jobs found in query for tenant 'example-tenant':",
+            self.output
+        )
+
+    def test_no_jobs_on_projects_query(self):
+        """Checks that no 'Jobs found in tenant...' string is printed for a
+        '--projects' query.
+        """
+        sys.argv = [
+            '',
+            '--config', 'tests/e2e/data/configs/zuul.yaml',
+            '-f', 'text',
+            '-vv',
+            '--tenants', '^(example-tenant)$',
+            '--projects'
+        ]
+
+        main()
+
+        self.assertNotIn(
+            "Total jobs found in query for tenant 'example-tenant':",
+            self.output
+        )
+
+
+class TestDefaults(EndToEndTest):
+    """Tests for default query values extracted from the configuration
+    file.
+    """
+    zuul = OpenDevZuulContainer()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.zuul.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.zuul.stop()
 
     def test_default_tenants(self):
         """Checks that the configuration file can define the default tenants
