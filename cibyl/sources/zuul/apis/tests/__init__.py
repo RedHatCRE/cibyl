1bbc8c81b30d3270b7cbf19a2ea6662a2dd72c15
Jorge San Emeterio <jsanemet@redhat.com>
2022-05-27 08:47:56 +0200

    Adding skeleton for test search.

---
+++ cibyl/sources/zuul/apis/tests/__init__.py
@@ -0,0 +1,58 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+from abc import ABC, abstractmethod
+from typing import List
+
+from cibyl.sources.zuul.api import ZuulBuildAPI
+from cibyl.models.ci.zuul.test import TestKind
+
+
+class Test:
+    kind: TestKind
+    name: str
+    result: str
+    duration: float
+    url: str = None
+
+
+class TestSuite:
+    name: str
+    url: str
+    tests: List[Test]
+
+
+class TestFinder(ABC):
+    @abstractmethod
+    def find(self, build):
+        """
+
+        :param build:
+        :type build: :class:`ZuulBuildAPI`
+        :return:
+        """
+        raise NotImplementedError
+
+
+def test_lookup(build, *finders):
+    """
+
+    :param build:
+    :type build: :class:`ZuulBuildAPI`
+    :param finders:
+    :type finders: TestFinder
+    :return:
+    """
+    pass
