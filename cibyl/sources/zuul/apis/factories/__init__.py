f6c1ab045d8cdac39f0c9fbf6bfb167a9d9fea1d
Jorge San Emeterio <jsanemet@redhat.com>
2022-06-02 09:19:35 +0200

    Fixing linter errors.

--- cibyl/sources/zuul/apis/factories/__init__.py
+++ cibyl/sources/zuul/apis/factories/__init__.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-from abc import abstractmethod, ABC
+from abc import ABC, abstractmethod
 
 
 class ZuulAPIFactory(ABC):
