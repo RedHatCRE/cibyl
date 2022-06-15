5e28b8697ec0874a39c3eb42521961593e75ad91
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-06-15 14:52:10 +0200

    Let each model class have their own set of plugins (#330)
    * Testing the problem.
    
    * Making each model have their own set of plugins.
    
    * Fixing the problem.
    
    * Fixing linter errors.

--- tests/unit/plugins/test_plugin.py
+++ tests/unit/plugins/test_plugin.py
@@ -15,7 +15,10 @@
 """
 
 from cibyl.exceptions.plugin import MissingPlugin
+from cibyl.models.ci.base.job import Job
+from cibyl.models.ci.zuul.job import Job as ZuulJob
 from cibyl.plugins import enable_plugins
+from cibyl.plugins.openstack import Plugin
 from tests.utils import RestoreAPIs
 
 
@@ -31,3 +34,30 @@ class TestPlugin(RestoreAPIs):
         self.assertRaises(MissingPlugin,
                           enable_plugins,
                           ["nonExistingPlugin"])
+
+    def test_extends_jenkins_job(self):
+        """Checks that the plugin extends the base job model.
+        """
+        plugin = Plugin()
+        plugin.extend_models()
+
+        # Plugin's of model are empty
+        self.assertIn('deployment', Job.plugin_attributes)
+
+    def test_extends_variant(self):
+        """Checks that the plugin extends the zuul variant model.
+        """
+        plugin = Plugin()
+        plugin.extend_models()
+
+        # Plugin's of model are empty
+        self.assertIn('deployment', ZuulJob.Variant.plugin_attributes)
+
+    def test_do_not_extend_zuul_job(self):
+        """Checks that the plugin does not extend a model it should not do.
+        """
+        plugin = Plugin()
+        plugin.extend_models()
+
+        # Plugin's of model are empty
+        self.assertFalse(ZuulJob.plugin_attributes)
