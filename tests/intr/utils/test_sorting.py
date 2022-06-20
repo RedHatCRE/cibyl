eed2f6fac3eac60d36ee0dc76c3dd7f12fd9d2dc
Jorge San Emeterio <jsanemet@redhat.com>
2022-06-20 09:15:56 +0200

    Fixing linter warnings.

--- tests/intr/utils/test_sorting.py
+++ tests/intr/utils/test_sorting.py
@@ -15,8 +15,8 @@
 """
 from unittest import TestCase
 
-from cibyl.utils.sorting import NativeComparator, BubbleSortAlgorithm, sort, \
-    nsort
+from cibyl.utils.sorting import (BubbleSortAlgorithm, NativeComparator, nsort,
+                                 sort)
 
 
 class TestNativeComparator(TestCase):
