45566d609b952e8ed9da86ef9d8563a350806740
Jorge San Emeterio <jsanemet@redhat.com>
2022-06-22 17:00:28 +0200

    Extracting formation of serialized data from its conversion to string.

--- cibyl/outputs/cli/ci/env/impl/serialized.py
+++ cibyl/outputs/cli/ci/env/impl/serialized.py
@@ -14,6 +14,7 @@
 #    under the License.
 """
 import json
+from abc import ABC
 
 from overrides import overrides
 
@@ -23,15 +24,20 @@ from cibyl.outputs.cli.printer import ColoredPrinter
 from cibyl.utils.colors import DefaultPalette
 
 
-class JSONPrinter(ColoredPrinter, CIPrinter):
+class SerializedDataPrinter(ColoredPrinter, CIPrinter, ABC):
     def __init__(self,
+                 dump_function,
                  query=QueryType.NONE,
                  verbosity=0,
-                 palette=DefaultPalette(),
-                 indentation=2):
+                 palette=DefaultPalette()):
+        """Constructor. See parent for more information.
+
+        :param dump_function:
+        :type dump_function: (dict) -> str
+        """
         super().__init__(query, verbosity, palette)
 
-        self._indentation = indentation
+        self._dump = dump_function
 
     @overrides
     def print_environment(self, env):
@@ -39,10 +45,26 @@ class JSONPrinter(ColoredPrinter, CIPrinter):
             'name': env.name.value
         }
 
-        return self._to_json(result)
+        return self._dump(result)
 
     def print_system(self, system):
         return ''
+
+
+class JSONPrinter(SerializedDataPrinter):
+    def __init__(self,
+                 query=QueryType.NONE,
+                 verbosity=0,
+                 palette=DefaultPalette(),
+                 indentation=2):
+        super().__init__(
+            dump_function=self._to_json,
+            query=query,
+            verbosity=verbosity,
+            palette=palette
+        )
+
+        self._indentation = indentation
 
     def _to_json(self, obj):
         return json.dumps(obj, indent=self._indentation)
