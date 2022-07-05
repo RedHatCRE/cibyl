48708e0a56541c921463bb5ecc2e50435061ce54
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-07-05 16:33:40 +0200

    Get 'infra_type' from TripleO's environment file (#382)
    * Moving cibyl tests to its own folder to make space for other modules.
    
    * Creating skeleton of library.
    
    * Moving GitHub library to be under TripleO module.
    
    * Adding new tests to Tox.
    
    * Delete conflicting file.
    
    * Adding utility to check if a string is a URL.
    
    * Defining a URL type that checks its validity on creation.
    
    * Using constructor to build default values.
    
    * Small change
    
    * Renaming utility module to avoid cyclic dependencies
    
    * Adding utilities to filter URLs.
    
    * Adding a utility that allows to get the repository from a Git URL.
    
    * Moving things around
    
    * Fixing linter warnings
    
    * Fixing tests.
    
    * Making a module for git related stuff.
    
    * Adding heat repository to outline
    
    * Defining errors for the library.
    
    * Fixing test case.
    
    * Adding a class that validates the insights input.
    
    * Starting work on the main class of the library.
    
    * Starting work on downloading a file from Git.
    
    * Implementing a basic downloader based on the GitHub API.
    
    * Avoiding cyclic dependencies
    
    * Better naming the GitHub API.
    
    * Fixing errors.
    
    * Getting the list of APIs that can provide for a URL
    
    * Trying to download file from quickstart
    
    * Removing useless setup step on GitHubDownloader.
    
    * Extracting the IP version from the featureset.
    
    * Start describing a schema for featuresets.
    
    * Adding a tool to face against JSON schemas.
    
    * Better type hinting the 'json' module.
    
    * Validating the featureset data before starting to use it.
    
    * Fixing linter warnings.
    
    * Deleting useless files.
    
    * Adding a utility that takes care of cloning a repository in order to access its contents.
    
    * Skeleton for downloader based on the Git CLI.
    
    * Adding the chance to open a repository already alive on the filesystem.
    
    * Moving location of 'types' module.
    
    * Making 'Path' its own class.
    
    * Renaming 'interfacing' module.
    
    * Using pathlib to handle paths.
    
    * Adding git cli as another option to retrieve files from.
    
    * Adding types to handle directories and files.
    
    * Fixing errors.
    
    * Testing types.
    
    * Small changes
    
    * Renaming heat repository variable.
    
    * Testing git utility.
    
    * Documenting the code.
    
    * Test FeatureSetInterpreter.
    
    * Testing git module.
    
    * Fixing linting.
    
    * Documenting the code.
    
    * Check that the path can resolve the '~' shortcut.
    
    * Defining plan for this field.
    
    * Adding an interpreter for the environment file.
    
    * Adding a base class for the interpreters.
    
    Contents of the constructor are the same on both instances, so I had them inherit a parent that takes care of that boilerplate.
    
    * Adding overrides to the interpreters.
    
    'overrides' is a complementary dictionary that allows the user to overwrite things from the file. If a file is found first on the 'overrides' dictionary, it is chosen before the value of the file.
    
    * Passing overrides to the interpreters.
    
    * Have TripleO return 'infra_type'.
    
    * Document the code.

--- tripleo/insights/exceptions.py
+++ tripleo/insights/exceptions.py
@@ -29,3 +29,8 @@ class DownloadError(InsightsError):
     """Indicates that a problem happened while a file was getting
     downloaded.
     """
+
+
+class IllegibleData(InsightsError):
+    """Some data is formatted on a way that the library cannot understand.
+    """
