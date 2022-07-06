44e65ad978db9d0157b5567579f0e13c67f4b312
Jorge San Emeterio <jsanemet@redhat.com>
2022-07-06 16:23:22 +0200

    Fixing linter warnings.

--- cibyl/plugins/openstack/sources/zuul/deployments/outlines.py
+++ cibyl/plugins/openstack/sources/zuul/deployments/outlines.py
@@ -13,9 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-from typing import Tuple, Any
-
 from dataclasses import dataclass
+from typing import Any, Tuple
 
 from cibyl.plugins.openstack.sources.zuul.tripleo import (
     QuickStartFileCreator, QuickStartPathCreator)
