2f60919b2b52265efc6d4f6c31a1d3528132fb19
Lukáš Piwowarski <33072434+lpiwowar@users.noreply.github.com>
2022-07-14 19:32:40 +0200

    Add type hints (#414)
    Add type hints to functions according to PEP-484 [1] in the
    outputs subpackage.
    
    This patch also removes now redundant type hints from
    docstrings in the modified files.
    
    [1] https://peps.python.org/pep-0484/

--- cibyl/outputs/cli/ci/system/common/builds.py
+++ cibyl/outputs/cli/ci/system/common/builds.py
@@ -13,32 +13,29 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
+from cibyl.models.ci.base.build import Build
 from cibyl.outputs.cli.ci.system.common.status import get_status_colored
+from cibyl.utils.colors import ColorPalette
 from cibyl.utils.strings import IndentedTextBuilder
 from cibyl.utils.time import as_minutes
 
 
-def has_status_section(build):
+def has_status_section(build: Build) -> bool:
     """Checks whether a build has enough data to build a status entry for
     its description.
 
     :param build: The build to check.
-    :type build: :class:`cibyl.models.ci.base.build.Build`
     :return: True if a status section can be built, False if not.
-    :rtype: bool
     """
     return build.status.value
 
 
-def get_status_section(palette, build):
+def get_status_section(palette: ColorPalette, build: Build) -> str:
     """Generates the text describing the status of a build.
 
     :param palette: The palette of colors to follow.
-    :type palette: :class:`cibyl.utils.colors.ColorPalette`
     :param build: The build to get the data from.
-    :type build: :class:`cibyl.models.ci.base.build.Build`
     :return: The text with the status of the build.
-    :rtype: str
     """
     if not has_status_section(build):
         raise ValueError('Build has no status to create a section for.')
@@ -51,27 +48,22 @@ def get_status_section(palette, build):
     return text.build()
 
 
-def has_duration_section(build):
+def has_duration_section(build: Build) -> bool:
     """Checks whether a build has enough data to build a duration entry for
     its description.
 
     :param build: The build to check.
-    :type build: :class:`cibyl.models.ci.base.build.Build`
     :return: True if a duration section can be built, False if not.
-    :rtype: bool
     """
     return build.duration.value
 
 
-def get_duration_section(palette, build):
+def get_duration_section(palette: ColorPalette, build: Build) -> str:
     """Generates the text describing the duration of a build.
 
     :param palette: The palette of colors to follow.
-    :type palette: :class:`cibyl.utils.colors.ColorPalette`
     :param build: The build to get the data from.
-    :type build: :class:`cibyl.models.ci.base.build.Build`
     :return: The text with the duration of the build.
-    :rtype: str
     """
     if not has_duration_section(build):
         raise ValueError('Build has no duration to create a section for.')
