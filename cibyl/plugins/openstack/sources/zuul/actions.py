aea6005f104ed4874170d44f98081635b2e299c7
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-07-15 08:43:51 +0200

    Make the deployment arguments only return what was asked. (#415)
    * Moving argument handler to its own module.
    
    * Moving DeploymentGenerator to its own module.
    
    * Changing meaning of a test case.
    
    * Adding a class that gets the deployment summary and applies some postprocessing to it.
    
    * Adding a class that checks if an argument should be part of a deployment or not.
    
    * The variant summary now also returns the release.
    
    * Integing all the tools together to fix the bug.
    
    * Testing DeploymentGenerator.
    
    * Testing ArgumentReview.
    
    * Finish testing.
    
    * Documenting the code.

--- cibyl/plugins/openstack/sources/zuul/actions.py
+++ cibyl/plugins/openstack/sources/zuul/actions.py
@@ -15,21 +15,15 @@
 """
 import logging
 from dataclasses import dataclass
-from enum import Enum
-from typing import Any
 
-from cibyl.plugins.openstack import Deployment
-from cibyl.plugins.openstack.network import Network
+from cibyl.plugins.openstack.sources.zuul.deployments.arguments import \
+    SpecArgumentHandler
 from cibyl.plugins.openstack.sources.zuul.deployments.filtering import \
     DeploymentFiltering
-from cibyl.plugins.openstack.sources.zuul.deployments.outlines import \
-    OutlineCreator
-from cibyl.plugins.openstack.sources.zuul.variants import ReleaseSearch
-from cibyl.plugins.openstack.storage import Storage
+from cibyl.plugins.openstack.sources.zuul.deployments.generator import \
+    DeploymentGenerator
 from cibyl.sources.zuul.output import QueryOutputBuilder
 from cibyl.sources.zuul.queries.jobs import perform_jobs_query
-from cibyl.sources.zuul.transactions import VariantResponse
-from tripleo.insights import DeploymentLookUp
 
 LOG = logging.getLogger(__name__)
 
@@ -56,140 +50,6 @@ def _default_variant_query(job, **kwargs):
     :rtype: list[:class:`cibyl.sources.zuul.transactions.VariantResponse`]
     """
     return job.variants().get()
-
-
-class SpecArgumentHandler:
-    """Figures out which argument holds the jobs to be fetched from the host.
-    """
-
-    class Option(Enum):
-        NONE = 0
-        """Could not determine which argument has the filters."""
-        EMPTY = 1
-        """Arguments were present, but no filter was defined."""
-        JOBS = 2
-        """Pick the filters on the 'jobs' arguments."""
-        SPEC = 3
-        """Pick the filters on the 'spec' arguments."""
-
-    def get_target_jobs(self, **kwargs):
-        """Determines the source for the target jobs.
-
-        The rules are:
-            - If neither argument is present -> None
-            - If just one of the two is present -> Pick that
-            - If both are present, but neither have a value -> No filter
-            - If both are present, but only one has a value -> Pick that
-            - If both are present and have a value -> Prefer '--spec'
-
-        :param kwargs: Tha arguments to study.
-        :key jobs: First candidate for provider of target jobs.
-            Type: Argument. Default: None.
-        :key spec: Second candidate for provider of target jobs.
-            Type: Argument. Default: None.
-        :return: Option to be taken relative to the provided arguments.
-        :rtype: :class:`SpecArgumentHandler.Option`
-        """
-        if 'jobs' not in kwargs:
-            if 'spec' not in kwargs:
-                # Neither of the two arguments were passed
-                return SpecArgumentHandler.Option.NONE
-
-            # Only 'spec' was passed
-            return SpecArgumentHandler.Option.SPEC
-
-        if 'spec' not in kwargs:
-            # Only 'jobs' was passed
-            return SpecArgumentHandler.Option.JOBS
-
-        if not kwargs['jobs']:
-            if not kwargs['spec']:
-                # Neither of the two arguments have a value
-                return SpecArgumentHandler.Option.EMPTY
-
-            # Only 'spec' has a value
-            return SpecArgumentHandler.Option.SPEC
-
-        if not kwargs['spec']:
-            # Only 'jobs' has a value
-            return SpecArgumentHandler.Option.JOBS
-
-        LOG.warning("Ignoring argument '--jobs' in favor of '--spec'.")
-
-        # If both have a value, prefer 'spec'
-        return SpecArgumentHandler.Option.SPEC
-
-
-class DeploymentGenerator:
-    """Factory for generation of :class:`Deployment`.
-    """
-
-    class Tools:
-        """Tools the factory will use to do its task.
-        """
-        outline_creator = OutlineCreator()
-        """Tests care of creating the TripleO outline for a Zuul job."""
-        deployment_lookup = DeploymentLookUp()
-        """Gets additional information on the deployment from TripleO."""
-        release_search = ReleaseSearch()
-        """Takes care of finding the release of the deployment."""
-
-    def __init__(self, tools: Tools = Tools()):
-        """Constructor.
-
-        :param tools: The tools this will use.
-        """
-        self._tools = tools
-
-    @property
-    def tools(self) -> Tools:
-        """
-        :return: The tools this will use.
-        """
-        return self._tools
-
-    def generate_deployment_for(
-        self,
-        variant: VariantResponse,
-        **kwargs: Any
-    ) -> Deployment:
-        """Creates a new deployment based on the data from a job's variant.
-
-        :param variant: The variant to fetch data from.
-        :return: The deployment.
-        """
-        summary = self.tools.deployment_lookup.run(
-            self.tools.outline_creator.new_outline_for(variant)
-        )
-
-        return Deployment(
-            release=self._get_release(variant, **kwargs),
-            infra_type=summary.infra_type,
-            topology=str(summary.topology),
-            storage=Storage(
-                cinder_backend=summary.cinder_backend
-            ),
-            network=Network(
-                ip_version=summary.ip_version
-            )
-        )
-
-    def _get_release(self, variant: VariantResponse, **kwargs: Any) -> str:
-        release_search = self.tools.release_search
-
-        if any(term in kwargs for term in ('spec', 'release')):
-            release = release_search.search(variant)
-
-            if not release:
-                # Fall back to the default value
-                return 'master'
-
-            _, value = release
-
-            return value
-
-        # Nothing means to not output this field.
-        return ''
 
 
 class DeploymentQuery:
---
+++ cibyl/plugins/openstack/sources/zuul/deployments/generator.py
@@ -0,0 +1,138 @@
+"""
+#    Copyright 2022 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from cibyl.plugins.openstack import Deployment
+from cibyl.plugins.openstack.network import Network
+from cibyl.plugins.openstack.sources.zuul.deployments.arguments import \
+    ArgumentReview
+from cibyl.plugins.openstack.sources.zuul.deployments.summary import (
+    VariantDeployment, VariantDeploymentFactory)
+from cibyl.plugins.openstack.storage import Storage
+from cibyl.sources.zuul.transactions import VariantResponse as Variant
+
+
+class DeploymentGenerator:
+    """Factory for generation of :class:`Deployment`.
+    """
+
+    @dataclass
+    class Tools:
+        """Tools the factory will use to do its task.
+        """
+        argument_review = ArgumentReview()
+        """Used to understand what the user desires."""
+        variant_summary = VariantDeploymentFactory()
+        """Used to get data on a variant's deployment."""
+
+    def __init__(self, tools: Tools = Tools()):
+        """Constructor.
+
+        :param tools: The tools this will use.
+        """
+        self._tools = tools
+
+    @property
+    def tools(self) -> Tools:
+        """
+        :return: The tools this will use.
+        """
+        return self._tools
+
+    def generate_deployment_for(
+        self,
+        variant: Variant,
+        **kwargs: Any
+    ) -> Deployment:
+        """Creates a new deployment based on the data from a job's variant.
+
+        :param variant: The variant to fetch data from.
+        :return: The deployment.
+        """
+        summary = self.tools.variant_summary.create_for(variant)
+
+        return Deployment(
+            release=self._get_release(summary, **kwargs),
+            infra_type=self._get_infra_type(summary, **kwargs),
+            topology=self._get_topology(summary, **kwargs),
+            storage=Storage(
+                cinder_backend=self._get_cinder_backend(summary, **kwargs)
+            ),
+            network=Network(
+                ip_version=self._get_ip_version(summary, **kwargs)
+            )
+        )
+
+    def _get_release(
+        self,
+        summary: VariantDeployment,
+        **kwargs: Any
+    ) -> Optional[str]:
+        arguments = self.tools.argument_review
+
+        if not arguments.is_release_requested(**kwargs):
+            return None
+
+        return summary.get_release()
+
+    def _get_infra_type(
+        self,
+        summary: VariantDeployment,
+        **kwargs: Any
+    ) -> Optional[str]:
+        arguments = self.tools.argument_review
+
+        if not arguments.is_infra_type_requested(**kwargs):
+            return None
+
+        return summary.get_infra_type()
+
+    def _get_topology(
+        self,
+        summary: VariantDeployment,
+        **kwargs: Any
+    ) -> Optional[str]:
+        arguments = self.tools.argument_review
+
+        if not arguments.is_topology_requested(**kwargs):
+            return None
+
+        return summary.get_topology()
+
+    def _get_cinder_backend(
+        self,
+        summary: VariantDeployment,
+        **kwargs: Any
+    ) -> Optional[str]:
+        arguments = self.tools.argument_review
+
+        if not arguments.is_cinder_backend_requested(**kwargs):
+            return None
+
+        return summary.get_cinder_backend()
+
+    def _get_ip_version(
+        self,
+        summary: VariantDeployment,
+        **kwargs: Any
+    ) -> Optional[str]:
+        arguments = self.tools.argument_review
+
+        if not arguments.is_ip_version_requested(**kwargs):
+            return None
+
+        return summary.get_ip_version()
