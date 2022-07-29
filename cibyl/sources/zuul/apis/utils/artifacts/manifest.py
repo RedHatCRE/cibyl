d335de2145159fe402837d57c690f9e385e8ed44
Jorge San Emeterio <jsanemet@redhat.com>
2022-07-29 15:21:51 +0200

    Documenting the code.

--- cibyl/sources/zuul/apis/utils/artifacts/manifest.py
+++ cibyl/sources/zuul/apis/utils/artifacts/manifest.py
@@ -26,36 +26,47 @@ from cibyl.utils.net import download_into_memory
 
 @dataclass
 class ManifestItem:
+    """Schema for one of the elements on the manifest file.
+    """
     name: str = field(
         metadata=dict(type='Element')
     )
+    """Name of the element."""
     mimetype: str = field(
         metadata=dict(type='Element')
     )
+    """Type of the element."""
     encoding: Optional[str] = field(
         default=None,
         metadata=dict(type='Element')
     )
+    """Name of the encoding in use, only for files.."""
     last_modified: Optional[int] = field(
         default=None,
         metadata=dict(type='Element')
     )
+    """Epoch time when the element was last changed."""
     size: Optional[int] = field(
         default=None,
         metadata=dict(type='Element')
     )
+    """Size of the element, only for files."""
     children: Optional[List['ManifestItem']] = field(
         default=None,
         metadata=dict(type='Element')
     )
+    """Other elements below this one, only for directories."""
 
 
 @dataclass
 class Manifest:
+    """Schema for the manifest file published by a Zuul build.
+    """
     tree: List[ManifestItem] = field(
         default_factory=list,
         metadata=dict(type='Element')
     )
+    """Tree containing all the logs published by the build."""
     index_links: bool = field(
         default=False,
         metadata=dict(type='Element')
@@ -63,24 +74,47 @@ class Manifest:
 
 
 class ManifestDownloader:
+    """Utility used to download the manifest published by different sources.
+    """
+
     class DownloadFromBuild:
+        """Utility used to retrieve the manifest from a Zuul build.
+        """
+
         def __init__(self, build: Build, tools: 'ManifestDownloader.Tools'):
+            """Constructor.
+
+            :param build: Build to get the manifest for.
+            :param tools: Tools used by this to perform its task.
+            """
             self._build = build
             self._tools = tools
 
         @property
         def build(self):
+            """
+            :return: Build to get the manifest for.
+            """
             return self._build
 
         @property
         def tools(self):
+            """
+            :return: Tools used by this to perform its task.
+            """
             return self._tools
 
         @property
         def session(self):
+            """
+            :return: Session this uses to perform HTTP tasks.
+            """
             return self._build.session.session
 
         def get_manifest(self) -> Manifest:
+            """
+            :return: The manifest published by the build.
+            """
             manifests = self._retrieve_manifests()
 
             if len(manifests) == 0:
@@ -115,16 +149,31 @@ class ManifestDownloader:
             )
 
     class Tools(NamedTuple):
+        """Tools used by this to perform its task.
+        """
         parser: JsonParser = JsonParser()
+        """Tool used to parse a JSON file into a Python object."""
 
     def __init__(self, tools: Tools = Tools()):
+        """Constructor.
+
+        :param tools: Tools this uses to perform its task.
+        """
         self._tools = tools
 
     @property
     def tools(self):
+        """
+        :return: Tools this uses to perform its task.
+        """
         return self._tools
 
     def download_from(self, build: Build) -> Manifest:
+        """Gets the manifest published by a particular build.
+
+        :param build: The build to get the manifest for.
+        :return: The manifest as a Python object.
+        """
         downloader = ManifestDownloader.DownloadFromBuild(build, self.tools)
 
         return downloader.get_manifest()
