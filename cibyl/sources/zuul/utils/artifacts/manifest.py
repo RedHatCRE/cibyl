dfda500f1516b0947efd70d39db8dbea0f63c9ac
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-08-02 14:41:18 +0200

    Adding a tool to search for tempests results on a zuul manifest (#447)
    * Defining the tempest test class.
    
    * Small changes
    
    * Adding tool to find the tempest results file amidst all the logs published by a build.
    
    * Documenting the code.
    
    * Renaming another docs folder to _docs
    
    * Moving utils outside the 'apis' folder.
    
    * Making search for a file on the manifest generic.
    
    * Documenting the code.
    
    * Testing the regex patterns.
    
    * Fixing linter errors.

--- cibyl/sources/zuul/utils/artifacts/manifest.py
+++ cibyl/sources/zuul/utils/artifacts/manifest.py
@@ -14,13 +14,14 @@
 #    under the License.
 """
 from dataclasses import dataclass, field
-from typing import List, NamedTuple, Optional, Sequence
+from typing import Iterable, List, NamedTuple, Optional, Sequence, Tuple
 
 from xsdata.formats.dataclass.parsers import JsonParser
 
 from cibyl.sources.zuul.apis import Artifact, ArtifactKind
 from cibyl.sources.zuul.apis.rest import ZuulBuildRESTClient as Build
 from cibyl.sources.zuul.utils.artifacts import ArtifactError
+from cibyl.utils.filtering import matches_regex
 from cibyl.utils.net import download_into_memory
 
 
@@ -173,7 +174,258 @@ class ManifestDownloader:
 
         :param build: The build to get the manifest for.
         :return: The manifest as a Python object.
+        :raises ArtifactError: If the build has no manifest or exposes
+                more than one.
         """
         downloader = ManifestDownloader.DownloadFromBuild(build, self.tools)
 
         return downloader.get_manifest()
+
+
+ManifestLevel = Iterable[ManifestItem]
+"""Represents a directory on the manifest."""
+
+
+class ManifestDigger:
+    """Tool used to transverse the directory tree of a manifest file. The
+    purpose of this tool is to be able to safely go down a branch on the
+    manifest's directory tree to its end. It allows to either observe the
+    contents of a level or to go deeper down the line. Once the end is
+    reached, this provides mechanism to be aware of so.
+    """
+
+    def __init__(self, manifest: Manifest):
+        """Constructor.
+
+        :param manifest: The manifest to dig through.
+        """
+        self._manifest = manifest
+        self._current_level = manifest.tree
+
+    @property
+    def manifest(self) -> Manifest:
+        """
+        :return: The manifest this is working with.
+        """
+        return self._manifest
+
+    @property
+    def current_level(self) -> ManifestLevel:
+        """
+        :return: Contents of the level the digger is currently at.
+        """
+        return self._current_level
+
+    def has_sublevel(self, name: str) -> bool:
+        """Checks whether there is a directory below this level with the
+        suggested name.
+
+        :param name: The name to check.
+        :return: True if there is, False if not.
+        """
+        return bool(self._get_sublevel(name))
+
+    def delve_into(self, level: str) -> None:
+        """Moves the digger down another level.
+
+        .. seealso::
+            :py:meth:`has_sublevel`
+                -> For checking if the level exists beforehand.
+
+        :param level: Name of the directory to go down to.
+        :raises ArtifactError: If the level does not exist.
+        """
+        if not self.has_sublevel(level):
+            msg = f"Manifest has no level: '{level}'."
+            raise ArtifactError(msg)
+
+        self._current_level = self._get_sublevel(level)
+
+    def _get_sublevel(self, level: str) -> Optional[ManifestLevel]:
+        """Gets the contents of a directory pending from the current level.
+
+        :param level: Name of the directory to search for.
+        :return: Contents of that directory. 'None' if it does not exist or
+            the name points to a file instead.
+        """
+        # Search for the sublevel
+        for item in self.current_level:
+            # Does the sublevel exist?
+            if item.name != level:
+                continue
+
+            # Check if at the end of the branch
+            if not item.children:
+                return None
+
+            # Not yet at the end -> Return the new level then
+            return item.children
+
+        return None
+
+
+class ManifestDiggerFactory:
+    """Factory for `ManifestDigger`.
+    """
+
+    def from_manifest(self, manifest: Manifest) -> ManifestDigger:
+        """Builds a new instance from a manifest file.
+
+        :param manifest: The manifest that the digger will work with.
+        :return: The digger's instance.
+        """
+        return ManifestDigger(manifest)
+
+
+class ManifestDir(str):
+    """Represents a path to a directory on a manifest file.
+    """
+
+    def __new__(cls, value: str) -> 'ManifestDir':
+        """Constructor.
+
+        The path is expected to be in a specific format for it to be
+        accepted. Some valid examples of this format are as follows:
+            - '/' -> Root of the manifest
+            - '/var/logs/tempest' -> A directory some levels deep
+
+        These other examples will not be accepted though:
+            - '/home/zuul/' -> Trailing '/' is forbidden
+            - 'home/logs' -> Must begin with a '/' character
+
+        :param value: The path to the directory.
+        :raises ValueError: If the path's format is invalid.
+        """
+        if not matches_regex(r'^(\/|(\/\w+)+)$', value):
+            raise ValueError(f"Invalid format for manifest path: '{value}'.")
+
+        return super().__new__(cls, value)
+
+
+class ManifestFile(str):
+    """Represents a path to a file on manifest file.
+    """
+
+    def __new__(cls, value: str) -> 'ManifestFile':
+        """Constructor.
+
+        The path is expected to be in a specific format for it to be
+        accepted. Some valid examples of this format are as follows:
+            - '/file.txt' -> File at the root of the manifest
+            - '/var/logs/tempest/results.xml' -> File on a subdir
+
+        These other examples will not be accepted though:
+            - 'home/file.txt' -> Must begin with a '/' character
+            - '/var/logs/file' -> Must have an extension
+
+        :param value: The path to the file.
+        :raises ValueError: If the path's format is invalid.
+        """
+        if not matches_regex(r'^(\/\w+)*(\/\w*\.\w*)$', value):
+            raise ValueError(f"Invalid format for manifest path: '{value}'.")
+
+        return super().__new__(cls, value)
+
+
+class ManifestFileSearch:
+    """Tool used to search for a certain file across a manifest's tree.
+    """
+
+    class SearchTerms(NamedTuple):
+        """Defines what the class has to search for.
+        """
+        paths: Iterable[ManifestDir]
+        """Collection of paths where the desired file may be. The class will
+        iterate through these in search of one of the desired files."""
+        files: Iterable[str]
+        """Collection of names the desired file may take. The class will
+        iterate through these at each path until one of them is found. The
+        first to be will be the one to be returned. Each of the files added
+        here must have an extension."""
+
+    class Tools(NamedTuple):
+        """Tools this uses to do its job.
+        """
+        diggers: ManifestDiggerFactory = ManifestDiggerFactory()
+        """Creates the walkers that will allow this to transverse the tree."""
+
+    def __init__(self, tools: Tools = Tools()):
+        """Constructor.
+
+        :param tools: The tools this will use to do its job.
+        """
+        self._tools = tools
+
+    @property
+    def tools(self):
+        """
+        :return: The tools this will use to do its job.
+        """
+        return self._tools
+
+    def find_in(
+        self,
+        manifest: Manifest,
+        search_terms: SearchTerms
+    ) -> Optional[Tuple[ManifestFile, ManifestItem]]:
+        """Searches the manifest file for the element defined in the search
+        terms.
+
+        :param manifest: The manifest file to search in.
+        :param search_terms: Defines what this will need to search for.
+        :return: A tuple containing on its first element the path to the file
+            and on its second the manifest entry that represents it. 'None'
+            if nothing were found.
+        """
+        for path in search_terms.paths:
+            level = self._get_manifest_level(manifest, path)
+
+            if not level:
+                # The file's directory does not exist
+                continue
+
+            for file in search_terms.files:
+                # See if the file is at this level
+                item = next(
+                    (item for item in level if item.name == file),
+                    None
+                )
+
+                if not item:
+                    # The file is not here
+                    continue
+
+                # Generate path to the file
+                return ManifestFile(f'{path}{file}'), item
+
+        return None
+
+    def _get_manifest_level(
+        self,
+        manifest: Manifest,
+        path: ManifestDir
+    ) -> Optional[ManifestLevel]:
+        """
+        :param manifest: The manifest to go through.
+        :param path: Path to the directory to get.
+        :return: Contents of the directory indicated by them. 'None' if the
+            directory could not be reached.
+        """
+        digger = self.tools.diggers.from_manifest(manifest)
+
+        for step in self._split_path(path):
+            if not digger.has_sublevel(step):
+                return None
+
+            digger.delve_into(step)
+
+        return digger.current_level
+
+    def _split_path(self, path: ManifestDir) -> Iterable[str]:
+        """
+        :param path: The path to divide.
+        :return: An ordered collection containing the names of the folders
+            that need to be cd'd in order to get to the one marked by the path.
+        """
+        # Remove initial '/' to avoid empty splits
+        return path[1:].split('/')
--- tests/cibyl/unit/sources/zuul/utils/artifacts/test_manifest.py
+++ tests/cibyl/unit/sources/zuul/utils/artifacts/test_manifest.py
@@ -20,7 +20,11 @@ import cibyl
 from cibyl.sources.zuul.apis import ArtifactKind
 from cibyl.sources.zuul.utils.artifacts import ArtifactError
 from cibyl.sources.zuul.utils.artifacts.manifest import (Manifest,
-                                                         ManifestDownloader)
+                                                         ManifestDigger,
+                                                         ManifestDir,
+                                                         ManifestDownloader,
+                                                         ManifestFile,
+                                                         ManifestFileSearch)
 
 
 class TestManifestDownloader(TestCase):
@@ -98,3 +102,193 @@ class TestManifestDownloader(TestCase):
 
         download.assert_called_once_with(url, session)
         tools.parser.from_string.assert_called_once_with(json, Manifest)
+
+
+class TestManifestDigger(TestCase):
+    """Tests for :class:`ManifestDigger`.
+    """
+
+    def test_error_on_invalid_level(self):
+        """Checks that an error is thrown in case the user tries to delve
+        into a level that does not exist.
+        """
+        level = 'some-level'
+
+        manifest = Mock()
+        manifest.tree = []
+
+        digger = ManifestDigger(manifest)
+
+        self.assertFalse(digger.has_sublevel(level))
+
+        with self.assertRaises(ArtifactError):
+            digger.delve_into(level)
+
+    def test_delves_into_level(self):
+        """Checks that it is capable to switch the current level if it does
+        exist.
+        """
+        level = 'some-level'
+
+        other = Mock()
+
+        item = Mock()
+        item.name = level
+        item.children = [other]
+
+        manifest = Mock()
+        manifest.tree = [item]
+
+        digger = ManifestDigger(manifest)
+
+        self.assertTrue(digger.has_sublevel(level))
+
+        digger.delve_into(level)
+
+        self.assertEqual([other], digger.current_level)
+
+
+class TestManifestDir(TestCase):
+    """Tests for :class:`ManifestDir`.
+    """
+
+    def test_regex_pattern(self):
+        """Checks that the regex pattern matches what the description says.
+        """
+        try:
+            ManifestDir('/')
+        except ValueError:
+            self.fail('Pattern is valid.')
+
+        try:
+            ManifestDir('/var/logs/tempest')
+        except ValueError:
+            self.fail('Pattern is valid.')
+
+        with self.assertRaises(ValueError):
+            ManifestDir('home/logs')
+
+        with self.assertRaises(ValueError):
+            ManifestDir('/home/zuul/')
+
+
+class TestManifestFile(TestCase):
+    """Tests for :class:`ManifestFile`.
+    """
+
+    def test_regex_pattern(self):
+        """Checks that the regex pattern matches what the description says.
+        """
+        try:
+            ManifestFile('/file.txt')
+        except ValueError:
+            self.fail('Pattern is valid.')
+
+        try:
+            ManifestFile('/var/logs/tempest/results.xml')
+        except ValueError:
+            self.fail('Pattern is valid.')
+
+        with self.assertRaises(ValueError):
+            ManifestFile('home/file.txt')
+
+        with self.assertRaises(ValueError):
+            ManifestFile('/var/logs/file')
+
+
+class TestManifestFileSearch(TestCase):
+    """Tests for :class:`ManifestFileSearch`.
+    """
+
+    def test_finds_results_on_manifest(self):
+        """Checks that it is capable of finding the results file in a
+        manifest that has it somewhere on its tree.
+        """
+        path = '/'
+
+        item = Mock()
+        item.name = 'item.txt'
+
+        manifest = Mock()
+        manifest.tree = [item]
+
+        digger = Mock()
+        digger.delve_into = Mock()
+        digger.has_sublevel = Mock()
+        digger.has_sublevel.return_value = True
+        digger.current_level = manifest.tree
+
+        search_terms = Mock()
+        search_terms.paths = (path,)
+        search_terms.files = (item.name,)
+
+        tools = Mock()
+        tools.diggers = Mock()
+        tools.diggers.from_manifest = Mock()
+        tools.diggers.from_manifest.return_value = digger
+
+        finder = ManifestFileSearch(tools=tools)
+
+        result = finder.find_in(manifest, search_terms)
+
+        self.assertIsNotNone(result)
+        self.assertEqual((f'{path}{item.name}', item), result)
+
+    def test_no_results_if_not_on_path(self):
+        """Checks returned value if the requested file is not at the
+        indicated level.
+        """
+        item = Mock()
+        item.name = 'item.txt'
+
+        manifest = Mock()
+        manifest.tree = [item]
+
+        digger = Mock()
+        digger.delve_into = Mock()
+        digger.has_sublevel = Mock()
+        digger.has_sublevel.return_value = True
+        digger.current_level = manifest.tree
+
+        search_terms = Mock()
+        search_terms.paths = ('/',)
+        search_terms.files = ('other.txt',)
+
+        tools = Mock()
+        tools.diggers = Mock()
+        tools.diggers.from_manifest = Mock()
+        tools.diggers.from_manifest.return_value = digger
+
+        finder = ManifestFileSearch(tools=tools)
+
+        result = finder.find_in(manifest, search_terms)
+
+        self.assertIsNone(result)
+
+    def test_no_results_if_no_path(self):
+        """Checks returned value if the indicated path does not exist on the
+        manifest.
+        """
+        manifest = Mock()
+        manifest.tree = []
+
+        digger = Mock()
+        digger.delve_into = Mock()
+        digger.has_sublevel = Mock()
+        digger.has_sublevel.return_value = True
+        digger.current_level = manifest.tree
+
+        search_terms = Mock()
+        search_terms.paths = ('/some/path',)
+        search_terms.files = ('other.txt',)
+
+        tools = Mock()
+        tools.diggers = Mock()
+        tools.diggers.from_manifest = Mock()
+        tools.diggers.from_manifest.return_value = digger
+
+        finder = ManifestFileSearch(tools=tools)
+
+        result = finder.find_in(manifest, search_terms)
+
+        self.assertIsNone(result)
