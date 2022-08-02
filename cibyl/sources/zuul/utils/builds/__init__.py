9b6d1da94d515c2247e48235a02e0ab53a768440
Jorge San Emeterio <jsanemet@redhat.com>
2022-08-02 12:47:08 +0200

    Adding a tool to generate the URL to the tempest results file.

--- cibyl/sources/zuul/utils/builds/__init__.py
+++ cibyl/sources/zuul/utils/builds/__init__.py
@@ -13,3 +13,23 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
+from cibyl.sources.zuul.apis import ZuulBuildAPI as Build
+from cibyl.utils.urls import URL
+
+
+def get_url_to_log_file(build: Build, file: str) -> URL:
+    """Generates the URL to a log file published by the build. No checks
+    are performed to ensure that the file belongs to the build or that the
+    URL points to an existing resource.
+
+    Examples:
+    >>> bld = Build(...)
+    ... get_url_to_log_file(bld, '/var/log/file.log')
+    'http://localhost:8080/log/server/var/log/file.log'
+
+    :param build: The build to use as reference.
+    :param file: Relative path to the log file starting from the root of the
+        build's manifest.
+    :return: URL where the file can be downloaded from.
+    """
+    return URL(f'{build.log_url}{file}')
