f945711c80ca308ce57c59a6db1ec6a333ddcade
Jorge San Emeterio <jsanemet@redhat.com>
2022-08-18 09:00:28 +0200

    Testing correct behaviour of verbose manager

--- tests/cibyl/intr/sources/zuul/managers/test_verbose.py
+++ tests/cibyl/intr/sources/zuul/managers/test_verbose.py
@@ -17,15 +17,17 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 from cibyl.cli.argument import Argument
-from cibyl.models.attribute import AttributeDictValue
 from cibyl.models.ci.zuul.build import Build
 from cibyl.models.ci.zuul.job import Job
 from cibyl.models.ci.zuul.pipeline import Pipeline
 from cibyl.models.ci.zuul.project import Project
 from cibyl.models.ci.zuul.tenant import Tenant
+from cibyl.models.ci.zuul.test import Test, TestKind, TestStatus
 from cibyl.models.ci.zuul.test_suite import TestSuite
 from cibyl.sources.zuul.managers.verbose import VerboseManager
 from cibyl.sources.zuul.source import Zuul
+from cibyl.sources.zuul.utils.tests.tempest.types import TempestTest
+from cibyl.sources.zuul.utils.tests.types import TestResult
 
 
 class TestVerboseManager(TestCase):
@@ -60,13 +62,11 @@ class TestVerboseManager(TestCase):
         result = source.get_tenants(**kwargs)
 
         models = result.value
+        expected = {
+            tenant.name: Tenant(name=tenant.name)
+        }
 
-        self.assertEqual(
-            {
-                tenant.name: Tenant(name=tenant.name)
-            },
-            models
-        )
+        self.assertEqual(expected, models)
 
     def test_projects_query(self):
         """Checks that projects are retrieved from the host when the argument
@@ -104,21 +104,19 @@ class TestVerboseManager(TestCase):
         result = source.get_projects(**kwargs)
 
         models = result.value
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                projects={
+                    project.name: Project(
+                        name=project.name,
+                        url=project.url
+                    )
+                }
+            )
+        }
 
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    projects={
-                        project.name: Project(
-                            name=project.name,
-                            url=project.url
-                        )
-                    }
-                )
-            },
-            models
-        )
+        self.assertEqual(expected, models)
 
     def test_pipelines_query(self):
         """Checks that pipelines are retrieved from the host when the argument
@@ -162,26 +160,24 @@ class TestVerboseManager(TestCase):
         result = source.get_pipelines(**kwargs)
 
         models = result.value
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                projects={
+                    project.name: Project(
+                        name=project.name,
+                        url=project.url,
+                        pipelines={
+                            pipeline.name: Pipeline(
+                                name=pipeline.name
+                            )
+                        }
+                    )
+                }
+            )
+        }
 
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    projects={
-                        project.name: Project(
-                            name=project.name,
-                            url=project.url,
-                            pipelines={
-                                pipeline.name: Pipeline(
-                                    name=pipeline.name
-                                )
-                            }
-                        )
-                    }
-                )
-            },
-            models
-        )
+        self.assertEqual(expected, models)
 
     def test_jobs_query(self):
         """Checks that jobs are retrieved from the host when the argument
@@ -221,21 +217,19 @@ class TestVerboseManager(TestCase):
         result = source.get_jobs(**kwargs)
 
         models = result.value
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                jobs={
+                    job.name: Job(
+                        name=job.name,
+                        url=job.url
+                    )
+                }
+            )
+        }
 
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    jobs={
-                        job.name: Job(
-                            name=job.name,
-                            url=job.url
-                        )
-                    }
-                )
-            },
-            models
-        )
+        self.assertEqual(expected, models)
 
     def test_variants_query(self):
         """Checks that variants are retrieved from the host when the argument
@@ -283,34 +277,32 @@ class TestVerboseManager(TestCase):
 
         kwargs = {
             'jobs': Argument('jobs', str, ''),
-            'variants': Argument('variants', None, '')
+            'variants': Argument('variants', str, '')
         }
 
         result = source.get_jobs(**kwargs)
 
         models = result.value
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                jobs={
+                    job.name: Job(
+                        name=job.name,
+                        url=job.url,
+                        variants=[
+                            Job.Variant(
+                                parent=variant.raw['parent'],
+                                name=variant.raw['name'],
+                                description=variant.raw['description']
+                            )
+                        ]
+                    )
+                }
+            )
+        }
 
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    jobs={
-                        job.name: Job(
-                            name=job.name,
-                            url=job.url,
-                            variants=[
-                                Job.Variant(
-                                    parent=variant.raw['parent'],
-                                    name=variant.raw['name'],
-                                    description=variant.raw['description']
-                                )
-                            ]
-                        )
-                    }
-                )
-            },
-            models
-        )
+        self.assertEqual(expected, models)
 
     def test_builds_query(self):
         """Checks that builds are retrieved from the host when the argument
@@ -359,35 +351,33 @@ class TestVerboseManager(TestCase):
         )
 
         kwargs = {'builds': Argument('builds', str, '')}
-        result = source.get_jobs(**kwargs)
+        result = source.get_builds(**kwargs)
 
         models = result.value
-
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    jobs={
-                        job.name: Job(
-                            name=job.name,
-                            url=job.url,
-                            builds={
-                                build.raw['uuid']: Build(
-                                    Build.Info(
-                                        project=build.raw['project'],
-                                        pipeline=build.raw['pipeline'],
-                                        uuid=build.raw['uuid'],
-                                        result=build.raw['result'],
-                                        duration=build.raw['duration']
-                                    )
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                jobs={
+                    job.name: Job(
+                        name=job.name,
+                        url=job.url,
+                        builds={
+                            build.raw['uuid']: Build(
+                                Build.Data(
+                                    project=build.raw['project'],
+                                    pipeline=build.raw['pipeline'],
+                                    uuid=build.raw['uuid'],
+                                    result=build.raw['result'],
+                                    duration=build.raw['duration']
                                 )
-                            }
-                        )
-                    }
-                )
-            },
-            models
-        )
+                            )
+                        }
+                    )
+                }
+            )
+        }
+
+        self.assertEqual(expected, models)
 
     def test_tests_query(self):
         """Checks that tests are retrieved from the host when the argument
@@ -397,7 +387,7 @@ class TestVerboseManager(TestCase):
         job = Mock()
         build = Mock()
         suite = Mock()
-        test = Mock()
+        test = Mock(spec=TempestTest)
 
         tenant.name = 'tenant'
         tenant.projects = Mock()
@@ -412,10 +402,30 @@ class TestVerboseManager(TestCase):
         job.builds.return_value = [build]
 
         build.job = job
+        build.raw = {
+            'project': 'project',
+            'pipeline': 'pipeline',
+            'uuid': '1234',
+            'result': 'success',
+            'duration': 10
+        }
         build.tests = Mock()
         build.tests.return_value = [suite]
 
+        suite.name = 'suite'
+        suite.url = 'http://localhost:8080/'
         suite.tests = [test]
+
+        test.name = 'test'
+        test.result = TestResult.SUCCESS
+        test.duration = 10
+        test.url = 'http://localhost:8080/'
+
+        data = Mock()
+        data.name = test.name
+        data.status = TestStatus.SUCCESS
+        data.duration = test.duration
+        data.url = test.url
 
         api = Mock()
         api.tenants = Mock()
@@ -435,32 +445,187 @@ class TestVerboseManager(TestCase):
         )
 
         kwargs = {'tests': Argument('tests', str, '')}
+        result = source.get_tests(**kwargs)
+
+        models = result.value
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                jobs={
+                    job.name: Job(
+                        name=job.name,
+                        url=job.url,
+                        builds={
+                            build.raw['uuid']: Build(
+                                Build.Data(
+                                    project=build.raw['project'],
+                                    pipeline=build.raw['pipeline'],
+                                    uuid=build.raw['uuid'],
+                                    result=build.raw['result'],
+                                    duration=build.raw['duration']
+                                ),
+                                suites=[
+                                    TestSuite(
+                                        TestSuite.Data(
+                                            name=suite.name,
+                                            url=suite.url,
+                                            tests=[
+                                                Test(
+                                                    kind=TestKind.TEMPEST,
+                                                    data=data
+                                                )
+                                            ]
+                                        )
+                                    )
+                                ]
+                            )
+                        }
+                    )
+                }
+            )
+        }
+
+        self.assertEqual(expected, models)
+
+    def test_mixed_tenants_projects(self):
+        """Checks that the combined answer of tenants and projects can be
+        returned.
+        """
+        tenant1 = Mock()
+        tenant2 = Mock()
+
+        project = Mock()
+
+        tenant1.name = 'tenant1'
+        tenant1.projects = Mock()
+        tenant1.projects.return_value = [project]
+
+        tenant2.name = 'tenant2'
+        tenant2.projects = Mock()
+        tenant2.projects.return_value = []
+
+        project.name = 'project'
+        project.tenant = tenant1
+
+        api = Mock()
+        api.tenants = Mock()
+        api.tenants.return_value = [tenant1, tenant2]
+
+        manager = VerboseManager(api)
+
+        factory = Mock()
+        factory.from_kwargs = Mock()
+        factory.from_kwargs.return_value = manager
+
+        source = Zuul(
+            name='test-source',
+            driver='zuul',
+            url='http://localhost:8080/',
+            manager=factory
+        )
+
+        kwargs = {
+            'tenants': Argument('tenants', str, ''),
+            'projects': Argument('projects', str, '')
+        }
+        result = source.get_projects(**kwargs)
+
+        models = result.value
+        expected = {
+            tenant1.name: Tenant(
+                name=tenant1.name,
+                projects={
+                    project.name: Project(
+                        name=project.name,
+                        url=project.url
+                    )
+                }
+            ),
+            tenant2.name: Tenant(
+                name=tenant2.name
+            )
+        }
+
+        self.assertEqual(expected, models)
+
+    def test_jobs_fetches_pipelines(self):
+        """Checks that looking for jobs will also get you the pipelines
+        they belong to.
+        """
+        tenant = Mock()
+        project = Mock()
+        pipeline = Mock()
+        job = Mock()
+
+        tenant.name = 'tenant'
+        tenant.projects = Mock()
+        tenant.projects.return_value = [project]
+        tenant.jobs = Mock()
+        tenant.jobs.return_value = [job]
+
+        project.name = 'project'
+        project.url = 'http://localhost:8080/'
+        project.tenant = tenant
+        project.pipelines = Mock()
+        project.pipelines.return_value = [pipeline]
+
+        pipeline.name = 'pipeline'
+        pipeline.project = project
+        pipeline.jobs = Mock()
+        pipeline.jobs.return_value = [job]
+
+        job.tenant = tenant
+        job.name = 'job'
+        job.url = 'http://localhost:8080/'
+
+        api = Mock()
+        api.tenants = Mock()
+        api.tenants.return_value = [tenant]
+
+        manager = VerboseManager(api)
+
+        factory = Mock()
+        factory.from_kwargs = Mock()
+        factory.from_kwargs.return_value = manager
+
+        source = Zuul(
+            name='test-source',
+            driver='zuul',
+            url='http://localhost:8080/',
+            manager=factory
+        )
+
+        kwargs = {'jobs': Argument('jobs', str, '')}
         result = source.get_jobs(**kwargs)
 
         models = result.value
-
-        self.assertEqual(
-            {
-                tenant.name: Tenant(
-                    name=tenant.name,
-                    jobs={
-                        job.name: Job(
-                            name=job.name,
-                            url=job.url,
-                            builds={
-                                build.raw['uuid']: Build(
-                                    Build.Info(
-                                        project=build.raw['project'],
-                                        pipeline=build.raw['pipeline'],
-                                        uuid=build.raw['uuid'],
-                                        result=build.raw['result'],
-                                        duration=build.raw['duration']
+        expected = {
+            tenant.name: Tenant(
+                name=tenant.name,
+                projects={
+                    project.name: Project(
+                        name=project.name,
+                        url=project.url,
+                        pipelines={
+                            pipeline.name: Pipeline(
+                                name=pipeline.name,
+                                jobs={
+                                    job.name: Job(
+                                        name=job.name,
+                                        url=job.url
                                     )
-                                )
-                            }
-                        )
-                    }
-                )
-            },
-            models
-        )
+                                }
+                            )
+                        }
+                    )
+                },
+                jobs={
+                    job.name: Job(
+                        name=job.name,
+                        url=job.url
+                    )
+                }
+            )
+        }
+
+        self.assertEqual(expected, models)
