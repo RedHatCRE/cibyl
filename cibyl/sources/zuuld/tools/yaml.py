269f6b3cdb4174b3ed3f8ef9325057168a82aa33
Jorge San Emeterio <jsanemet@redhat.com>
2022-11-10 09:39:26 +0100

    Documenting the code.

--- cibyl/sources/zuuld/tools/yaml.py
+++ cibyl/sources/zuuld/tools/yaml.py
@@ -26,9 +26,18 @@ LOG = logging.getLogger(__name__)
 
 
 class ZuulDFile(YAMLFile):
+    """Representation of a YAML file that meets the Zuul.D schema.
+    """
     SCHEMA = File('_data/schemas/zuuld.json')
+    """Location of the Zuul.D schema."""
 
     def __init__(self, file: File, tools: Optional[YAMLFile.Tools] = None):
+        """Constructor.
+
+        :param file: File to test against the Zuul.D schema.
+        :param tools: Tools this uses to do its task.
+        :raises YAMLError: If the file does not meet the schema.
+        """
         super().__init__(
             file=file,
             schema=ZuulDFile.SCHEMA,
@@ -37,30 +46,67 @@ class ZuulDFile(YAMLFile):
 
 
 class ZuulDFileFactory:
+    """Factory for :class:`ZuulDFile`.
+    """
+
     def from_file(self, file: File) -> ZuulDFile:
+        """Builds a new Zuul.D file from a generic one.
+
+        :param file: File that will be tested to see if it is a Zuul.D file.
+        :return: The given file, this time caster to a Zuul.D one.
+        :raises YAMLError: If the file does not meet the Zuul.D criteria.
+        """
         return ZuulDFile(file)
 
 
 class YAMLReader:
+    """Parses the contents of a Zuul.D file.
+    """
+
     class DataExtractor:
+        """Utility class used to extract YAML data from a Zuul.D file.
+        """
+
         def __init__(self, file: ZuulDFile):
+            """Constructor.
+
+            :param file: The file to get the data from.
+            """
             self._file = file
 
         @property
         def file(self) -> ZuulDFile:
+            """
+            :return: File this class is extracting data from.
+            """
             return self._file
 
         def jobs(self) -> YAMLArray:
+            """
+            :return: All 'job' entries found on the file.
+            """
             return [entry['job'] for entry in self.file.data if 'job' in entry]
 
     def __init__(self, file: ZuulDFile):
+        """Constructor.
+
+        :param file: The file to parse.
+        """
         self._data = YAMLReader.DataExtractor(file)
 
     @property
     def file(self) -> ZuulDFile:
+        """
+        :return: The file this class is parsing.
+        """
         return self._data.file
 
     def jobs(self) -> Iterable[Job]:
+        """Parses all job entries on the file and returns a python
+        representation for each of them.
+
+        :return: Model for each job entry on the file.
+        """
         result = []
 
         for job in self._data.jobs():
@@ -92,11 +138,22 @@ class YAMLReaderFactory:
     """
 
     def from_file(self, file: ZuulDFile) -> YAMLReader:
+        """Builds a new reader from a Zuul.D file.
+
+        :param file: The Zuul.D file.
+        :return: The reader instance.
+        """
         return YAMLReader(file)
 
 
 class YAMLSearch:
-    """Recursively looks for YAML files on a directory.
+    """Recursively looks for Zuul.D YAML files on a directory.
+
+    The class crawls first through a directory in search of generic YAML
+    files to test. Then, it checks each one in order to look for those
+    that can be considered Zuul.D compliant and finally returns those.
+
+    A Zuul.D compliant YAML file are those files that meet the Zuul.D schema.
     """
     DEFAULT_YAML_EXTENSIONS = ('.yml', '.yaml')
     """Default file extensions that identify YAML files."""
@@ -112,6 +169,7 @@ class YAMLSearch:
         files: ZuulDFileFactory = field(
             default_factory=lambda *_: ZuulDFileFactory()
         )
+        """Used to go from a generic YAML file to a Zuul.D compliant one."""
 
     def __init__(
         self,
@@ -122,7 +180,11 @@ class YAMLSearch:
 
         :param extensions:
             File extensions that identify YAML files.
-            'None' to use the default set: YAMLReader.DEFAULT_YAML_EXTENSIONS
+            These point to all files on a directory that are to be
+            checked for Zuul.D contents.
+            Consequently, files identified by these extensions may, or not, be
+            Zuul.D compliant.
+            'None' to use the default set: YAMLReader.DEFAULT_YAML_EXTENSIONS.
         :param tools:
             Tools used by the class to do its job.
             'None' to let this build its own.
@@ -139,23 +201,22 @@ class YAMLSearch:
     @property
     def extensions(self) -> Iterable[str]:
         """
-        :return: File extensions the class look for.
+        :return: File extensions the class looks for.
         """
         return self._extensions
 
     @property
     def tools(self) -> Tools:
         """
-        :return: Tools used by this to do its task.
+        :return: Tools used by the class to do its job.
         """
         return self._tools
 
     def search(self, path: Dir) -> Iterable[ZuulDFile]:
-        """Recursively searches the directory for all YAML files contained
-        within.
+        """Recursively searches the directory for all Zuul.D YAML files on it.
 
         :param path: Directory to look in.
-        :return: A handle to all retrieved files.
+        :return: A handle to all found files.
         """
         result = []
 
@@ -172,7 +233,12 @@ class YAMLSearch:
 
         return result
 
-    def _search_for_yamls_at(self, path: Dir):
+    def _search_for_yamls_at(self, path: Dir) -> Iterable[File]:
+        """Recursively looks for all YAML files on a directory.
+
+        :param path: Directory to look in.
+        :return: A handle to all found files.
+        """
         search = self.tools.searches.from_root(path)
         search.with_recursion()
 
