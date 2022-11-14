351f4e0c7c470572347f4061a69f0e18cf2817b3
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-11-14 12:24:38 +0100

    Fixing a bug where url validation stopped gerrit urls from being usable (#547)
    Accept Gerrit URLs by removing URL validation.

--- kernel/tools/urls.py
+++ kernel/tools/urls.py
@@ -47,7 +47,7 @@ def is_git(url: URL) -> bool:
     :param url: The URL to test.
     :return: True if the URL points to a git repository, False if not.
     """
-    return url.endswith('.git')
+    return True
 
 
 def is_github(url: URL) -> bool:
