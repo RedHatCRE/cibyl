316473a20dedb5ced35dc23cac839197bc1ae99b
Jorge San Emeterio <97454609+jsanemet@users.noreply.github.com>
2022-11-28 12:10:30 +0100

    Retrieving manila backend from scenario file (#552)
    * Adding manila backend to the scenario interpreter.
    
    * Returning manila backend as part of the summary.
    
    * Removing ubuntu-latest as build image.

--- tripleo/insights/lookup.py
+++ tripleo/insights/lookup.py
@@ -283,6 +283,7 @@ class DeploymentLookUp:
 
             result.components.cinder.backend = scenario.get_cinder_backend()
             result.components.glance.backend = scenario.get_glance_backend()
+            result.components.manila.backend = scenario.get_manila_backend()
             result.components.neutron.backend = scenario.get_neutron_backend()
             result.components.neutron.ml2_driver = scenario.get_ml2_driver()
 
